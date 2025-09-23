ad1fc31ab065417afba67d6c11c7157883735943
Elliot Smith <smith.elliot.ann@gmail.com>
2025-09-23 15:05:57 -0700

    Initial commit: SCENIC analysis pipeline

--- scenic_scripts/60_aucell_patched.sh
+++ scenic_scripts/60_aucell_patched.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/60_aucell_patched_%j.out
 #SBATCH --error=logs/60_aucell_patched_%j.err
 #SBATCH --time=3:59:00
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=htc
 
